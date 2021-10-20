cca208a872fa4e4466af61dfa0a7d708bca7eea6
lcierra <lcierra@student.21-school.ru>
2021-10-20 21:07:19 +0300

    initial commit

---
+++ BFY/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BFY
+//
+//  Created by Влада Приходченко on 20.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+}
+
