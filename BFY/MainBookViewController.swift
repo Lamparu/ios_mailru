f2f78a59d6f86e72be8ca0456f3af63661908d77
Vlada <lamparuv@mail.ru>
2021-11-20 15:45:32 +0300

    rm: vlada.xcurstate; auth + book

--- BFY/MainBookViewController.swift
+++ BFY/MainBookViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+
 class MainBookViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
@@ -16,7 +17,7 @@ class MainBookViewController: UIViewController {
     }()
     
     let bookImage: UIImageView = {
-        let imageName = "bookImage.jpeg" //подгуржать с бэкэнда
+        let imageName = "bookImage.png" //подгуржать с бэкэнда
         let image = UIImage(named: imageName)
         let imageView = UIImageView(image: image)
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -39,22 +40,22 @@ class MainBookViewController: UIViewController {
     } ()
     
     let stringBookName: UILabel = {
-        let text = "Капитанская дочка"
+        let text = "Гордость и предубеждение"
         let str = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
         str.text = text
         str.textColor = UIColor.black
-        str.font = UIFont(name: "AppleSDGothicNeo-Light", size: 24)
+        str.font = UIFont(name: "AppleSDGothicNeo-Light", size: 26)
         str.textAlignment = .center
         str.translatesAutoresizingMaskIntoConstraints = false
         return str
     }()
     
     let stringBookAuthor: UILabel = {
-        let text = "А. С. Пушкин"
-        let str = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        let text = "Джейн Остен"
+        let str = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 24))
         str.text = text
         str.textColor = UIColor.darkGray
-        str.font = UIFont(name: "AppleSDGothicNeo-Light", size: 24)
+        str.font = UIFont(name: "AppleSDGothicNeo-Light", size: 26)
         str.textAlignment = .center
         str.translatesAutoresizingMaskIntoConstraints = false
         return str
@@ -66,6 +67,7 @@ class MainBookViewController: UIViewController {
     textField.placeholder = "\(list) стр.";// str (число из бэка данные по этой книге)
     textField.font = UIFont(name: "AppleSDGothicNeo-Light", size: 25)
     textField.borderStyle = UITextField.BorderStyle.roundedRect
+    textField.textAlignment = .center
     textField.layer.cornerRadius = 20
     textField.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner]
     textField.layer.borderColor = UIColor(rgb: 0x6A7F60).cgColor
@@ -79,11 +81,11 @@ class MainBookViewController: UIViewController {
     
     
     let stringToListsField: UILabel = {
-        let text = "Останавились на"
+        let text = "Остановились на"
         let str = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
         str.text = text
-        str.textColor = UIColor(rgb: 0x6A7F60)
-        str.font = UIFont(name: "AppleSDGothicNeo-Light", size: 20)
+        str.textColor = UIColor.black
+        str.font = UIFont(name: "AppleSDGothicNeo-Light", size: 24)
         str.textAlignment = .center
         str.translatesAutoresizingMaskIntoConstraints = false
         return str
@@ -111,6 +113,11 @@ class MainBookViewController: UIViewController {
         applyShadowOnButtons(button: playButton)
 
         // Do any additional setup after loading the view.
+        
+        let backButton = UIBarButtonItem()
+        backButton.title = ""
+        backButton.tintColor = UIColor(rgb: 0x6A7F60)
+        self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
     }
     
     func createMainBookConstraint(){
@@ -121,45 +128,45 @@ class MainBookViewController: UIViewController {
     }
     
     func createbookImageConstraint(){
-        bookImage.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        bookImage.heightAnchor.constraint(equalToConstant: 263).isActive = true
-        bookImage.centerYAnchor.constraint(equalTo: mainFrame.centerYAnchor, constant: -70).isActive = true
+        bookImage.widthAnchor.constraint(equalToConstant: bookImage.frame.width / 2).isActive = true
+        bookImage.heightAnchor.constraint(equalToConstant: bookImage.frame.height / 2).isActive = true
+        bookImage.topAnchor.constraint(equalTo: mainFrame.topAnchor, constant: 30).isActive = true
         bookImage.centerXAnchor.constraint(equalTo: mainFrame.centerXAnchor).isActive = true
         
     }
     func createstringBookNameConstraint() {
         stringBookName.centerXAnchor.constraint(equalTo: bookImage.centerXAnchor).isActive = true
-        stringBookName.centerYAnchor.constraint(equalTo: bookImage.bottomAnchor, constant: 20).isActive = true
+        stringBookName.centerYAnchor.constraint(equalTo: bookImage.bottomAnchor, constant: 30).isActive = true
         
     }
     
     func createstringBookAuthorConstraint() {
         stringBookAuthor.centerXAnchor.constraint(equalTo: stringBookName.centerXAnchor).isActive = true
-        stringBookAuthor.centerYAnchor.constraint(equalTo: stringBookName.bottomAnchor, constant: 10).isActive = true
+        stringBookAuthor.centerYAnchor.constraint(equalTo: stringBookName.bottomAnchor, constant: 20).isActive = true
         
     }
     
     func createNumberOfListsFieldConstraint() {
         numberOfListsField.centerXAnchor.constraint(equalTo: stringBookAuthor.centerXAnchor).isActive = true
-        numberOfListsField.centerYAnchor.constraint(equalTo: stringBookAuthor.bottomAnchor, constant: 50).isActive = true
+        numberOfListsField.bottomAnchor.constraint(equalTo: mainFrame.bottomAnchor, constant: -30).isActive = true
         
     }
     
     func createstringToListsFieldConstraint() {
         stringToListsField.centerXAnchor.constraint(equalTo: numberOfListsField.centerXAnchor).isActive = true
-        stringToListsField.centerYAnchor.constraint(equalTo: numberOfListsField.topAnchor, constant: -10).isActive = true
+        stringToListsField.centerYAnchor.constraint(equalTo: numberOfListsField.topAnchor, constant: -20).isActive = true
         
     }
 
     
     @objc private func didTapRegButton(_ sender: UIButton) {
-            let Timer = TimerViewContoller()
-//           let navController = UINavigationController(rootViewController: Timer)
-//            self.present(navController, animated: true, completion: nil)
-            self.navigationController?.pushViewController(Timer, animated: true)
-            
-            
-        }
+               let Timer = TimerViewContoller()
+   //           let navController = UINavigationController(rootViewController: Timer)
+   //            self.present(navController, animated: true, completion: nil)
+               self.navigationController?.pushViewController(Timer, animated: true)
+               
+               
+           }
     
     func applyShadowOnButtons(button: UIButton) {
             button.layer.shadowColor = UIColor.black.cgColor
@@ -176,4 +183,3 @@ class MainBookViewController: UIViewController {
 
 
 }
-
--- BFY/TimerViewController.swift
+++ BFY/TimerViewController.swift
@@ -4,29 +4,34 @@
 //
 //  Created by Полина Подчуфарова on 15.11.2021.
 //
-
 import UIKit
+
+func maskRoundedImage(image: UIImage, radius: CGFloat) -> UIImage {
+       let imageView: UIImageView = UIImageView(image: image)
+       let layer = imageView.layer
+       layer.masksToBounds = true
+       layer.cornerRadius = radius
+       UIGraphicsBeginImageContext(imageView.bounds.size)
+       layer.render(in: UIGraphicsGetCurrentContext()!)
+       let roundedImage = UIGraphicsGetImageFromCurrentImageContext()
+       UIGraphicsEndImageContext()
+       return roundedImage!
+   }
 
 class TimerViewContoller: UIViewController {
     
     let stopwatchButton: UIButton = {
         let Button = UIButton()
-     //   Button.addTarget(self, action: #selector(didTapRegButton), for: .touchUpInside)
-        Button.setImage(UIImage(named: "stopwatch_off.png"), for: .normal)
-        Button.setImage(UIImage(named: "stopwatch_on.png"), for: .selected)
-    //    Button.layer.cornerRadius=50
-    //    Button.layer.masksToBounds = true
+        Button.addTarget(self, action: #selector(didTapTimerButton), for: .valueChanged)
+        Button.setImage(UIImage(named: "stopwatch.png"), for: .normal)
         Button.translatesAutoresizingMaskIntoConstraints = false
         return Button
     } ()
     
     let timerButton: UIButton = {
         let Button = UIButton()
-     //   Button.addTarget(self, action: #selector(didTapRegButton), for: .touchUpInside)
-        Button.setImage(UIImage(named: "timer_off.png"), for: .normal)
-        Button.setImage(UIImage(named: "timer_on.png"), for: .selected)
-    //    Button.layer.cornerRadius=50
-    //    Button.layer.masksToBounds = true
+        Button.addTarget(self, action: #selector(didTapTimerButton), for: .valueChanged)
+        Button.setImage(UIImage(named: "timer.png"), for: .normal)
         Button.translatesAutoresizingMaskIntoConstraints = false
         return Button
     } ()
@@ -37,35 +42,60 @@ class TimerViewContoller: UIViewController {
         view.backgroundColor = UIColor(rgb: 0x6A7F60)
         self.view.addSubview(stopwatchButton)
         applystopwhatchButtons(button: stopwatchButton)
+        applyShadowOnButtons(button: stopwatchButton)
         
         self.view.addSubview(timerButton)
         applytimerButtons(button: timerButton)
+        applyShadowOnButtons(button: timerButton)
+        
+        
+        let backButton = UIBarButtonItem()
+        backButton.title = ""
+        backButton.tintColor = UIColor(rgb: 0xfffcf4)
+        self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
 
         // Do any additional setup after loading the view.
     }
-    func applystopwhatchButtons(button: UIButton)
-        {
+    func applystopwhatchButtons(button: UIButton) {
             button.layer.shadowColor = UIColor.black.cgColor
-            button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -80 ).isActive = true
+            button.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 260 ).isActive = true
             button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -250 ).isActive = true
-            button.heightAnchor.constraint(equalToConstant: 120).isActive = true
-            button.widthAnchor.constraint(equalToConstant: 140).isActive = true
+            button.heightAnchor.constraint(equalToConstant: 96).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 96).isActive = true
+   
         }
     
-    func applytimerButtons(button: UIButton)
-        {
+    func applytimerButtons(button: UIButton) {
             button.layer.shadowColor = UIColor.black.cgColor
-            button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 80 ).isActive = true
+            button.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 34 ).isActive = true
             button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -250 ).isActive = true
-            button.heightAnchor.constraint(equalToConstant: 120).isActive = true
-            button.widthAnchor.constraint(equalToConstant: 140).isActive = true
+            button.heightAnchor.constraint(equalToConstant: 96).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 96).isActive = true
         }
+    
+    @objc private func didTapStopwatchButton(_ sender: UIButton) {
+             //  let Timer = TimerViewContoller()
+              // self.navigationController?.pushViewController(Timer, animated: true)
+           }
+    
+    @objc private func didTapTimerButton(_ sender: UIButton) {
+               //let Timer = TimerViewContoller()
+              // self.navigationController?.pushViewController(Timer, animated: true)
+               
+               
+           }
+     func applyShadowOnButtons(button: UIButton) {
+         button.layer.shadowColor = UIColor.black.cgColor
+         button.layer.shadowOffset = CGSize(width: 0, height: 3)
+         button.layer.shadowOpacity = 0.6
+         button.layer.shadowRadius = 3
+         button.layer.masksToBounds = false
+     }
     
     
 
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
